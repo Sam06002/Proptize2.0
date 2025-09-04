6a73f0d4ded22510c16fb3e1bb37f7100022313c
Shivam Srivastava <47245047+Sam06002@users.noreply.github.com>
2025-09-04 11:25:58 +0530

    Update deployment configuration for Streamlit Cloud

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-python -m spacy download en_core_web_sm
-chmod +x setup.sh
+set -e
+
+# Install the spaCy model
+python -m pip install --no-cache-dir -r requirements.txt
+python -m spacy download en_core_web_sm --no-cache-dir
